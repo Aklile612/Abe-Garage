57a582cf11abe41347ccda1036b6a80100ebcab4
Aklile Ansa <aklilemente72@gmail.com>
2025-04-21 19:28:10 +0300

    added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,18 @@
 import { useState } from 'react'
 import './App.css'
-import Header from './Components/Header/Header'
-import Footer from './Components/Footer/Footer'
+import { Routes,Route, useNavigate } from 'react-router-dom'
+import Home from './Components/Home/Home'
 
 function App() {
   
 
   return (
-    <div className="flex flex-col min-h-screen">
-      <Header />
-
-      {/* Main content area */}
-      
-
-      <Footer />
-    </div>
+      <>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+        </Routes>
+      </>
+   
   )
 }
 
