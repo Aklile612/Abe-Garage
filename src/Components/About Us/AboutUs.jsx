bd0d405edd403639d8c6b6fde5b5efe6d3159f54
Aklile Ansa <aklilemente72@gmail.com>
2025-04-22 17:17:46 +0300

    created about page

---
+++ src/Components/About Us/AboutUs.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Header from '../Header/Header'
+import Footer from '../Footer/Footer'
+
+const AboutUs = () => {
+  return (
+    <>
+    <Header/>
+
+
+
+
+
+
+    about us
+
+
+
+
+    <Footer/>
+      
+    </>
+  )
+}
+
+export default AboutUs
