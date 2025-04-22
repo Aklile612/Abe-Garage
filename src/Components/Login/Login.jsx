c9480cd54bae46bc85387be2ea2dc794ee0ea9d8
Aklile Ansa <aklilemente72@gmail.com>
2025-04-22 16:21:02 +0300

    Login page created

---
+++ src/Components/Login/Login.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import Header from '../Header/Header'
+import Footer from '../Footer/Footer'
+
+const Login = () => {
+  return (
+    <>
+    <Header/>
+      <h2>Login</h2>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <Footer/>
+    </>
+  )
+}
+
+export default Login
