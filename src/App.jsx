8240d01933c7b59eea672012d890619718e96f47
Aklile Ansa <aklilemente72@gmail.com>
2025-04-22 16:20:15 +0300

    added the path

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import { Routes,Route, useNavigate } from 'react-router-dom'
 import Home from './Components/Home/Home'
+import Login from './Components/Login/Login'
 
 function App() {
   
@@ -10,6 +11,7 @@ function App() {
       <>
         <Routes>
           <Route path='/' element={<Home/>}/>
+          <Route path='/login' element={<Login/>}/>
         </Routes>
       </>
    
