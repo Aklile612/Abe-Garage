cecb8be6cdba526366b4880b674b94e7afcb481c
Aklile Ansa <aklilemente72@gmail.com>
2025-04-22 18:28:39 +0300

    created contact us page

---
+++ src/Components/Contact Us/Contact.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import Header from '../Header/Header'
+import Footer from '../Footer/Footer'
+
+const Contact = () => {
+  return (
+    <>
+        <Header/>
+
+        Contact us
+
+
+
+
+
+
+
+
+
+
+
+
+
+        <Footer/> 
+    </>
+  )
+}
+
+export default Contact
