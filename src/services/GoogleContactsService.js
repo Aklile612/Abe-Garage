41df0fdadc73871290591185afa91b24ea2c6fe7
Aklile612 <aklilemente72@gmail.com>
2025-09-12 18:00:00 +0300

    style: refine card elevation and shadows

--- src/services/GoogleContactsService.js
+++ src/services/GoogleContactsService.js
@@ -176,3 +176,4 @@ export async function searchContacts(accessToken, query) {
 // style: refine card elevation and shadows 5
 // style: refine card elevation and shadows 6
 // style: refine card elevation and shadows 7
+// style: refine card elevation and shadows 8
