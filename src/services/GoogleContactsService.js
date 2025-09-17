3bad9e4c2eca5f608032d51db25a419cdfd6b9bc
Aklile612 <aklilemente72@gmail.com>
2025-09-17 04:00:00 +0300

    feat: add contact birthday reminders

--- src/services/GoogleContactsService.js
+++ src/services/GoogleContactsService.js
@@ -254,3 +254,4 @@ export async function searchContacts(accessToken, query) {
 // feat: add contact birthday reminders 1
 // feat: add contact birthday reminders 2
 // feat: add contact birthday reminders 3
+// feat: add contact birthday reminders 4
