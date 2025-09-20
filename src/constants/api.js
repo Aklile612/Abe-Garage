d9ff471042ceff7db25567a1a9db845bb3326c45
Aklile612 <aklilemente72@gmail.com>
2025-09-20 09:00:00 +0300

    feat: add API constants for Google services

---
+++ src/constants/api.js
@@ -0,0 +1,15 @@
+export const API = {
+  GOOGLE_CLIENT_ID_IOS: "YOUR_IOS_CLIENT_ID.apps.googleusercontent.com",
+  GOOGLE_CLIENT_ID_ANDROID: "YOUR_ANDROID_CLIENT_ID.apps.googleusercontent.com",
+  GOOGLE_AUTH_URL: "https://accounts.google.com/o/oauth2/v2/auth",
+  GOOGLE_TOKEN_URL: "https://oauth2.googleapis.com/token",
+  GOOGLE_REVOKE_URL: "https://oauth2.googleapis.com/revoke",
+  PEOPLE_API_BASE: "https://people.googleapis.com/v1",
+  USER_INFO_URL: "https://www.googleapis.com/oauth2/v2/userinfo",
+  SCOPES: [
+    "https://www.googleapis.com/auth/contacts",
+    "https://www.googleapis.com/auth/contacts.readonly",
+    "profile",
+    "email",
+  ],
+};
