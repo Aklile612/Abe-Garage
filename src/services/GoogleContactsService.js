4a63512d0883e75f6122ffc96308a7dd2ea0e825
Aklile612 <aklilemente72@gmail.com>
2025-09-29 05:00:00 +0300

    feat: add contact search by email

--- src/services/GoogleContactsService.js
+++ src/services/GoogleContactsService.js
@@ -248,3 +248,4 @@ export async function searchContacts(accessToken, query) {
 // feat: add contact search by email 2
 // feat: add contact search by email 3
 // feat: add contact search by email 4
+// feat: add contact search by email 5
