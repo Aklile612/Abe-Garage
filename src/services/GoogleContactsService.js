1e82f6ea76b7284a2ce5966a9b902a79074f621b
Aklile612 <aklilemente72@gmail.com>
2025-10-02 11:00:00 +0300

    feat: add favorites section to contacts

--- src/services/GoogleContactsService.js
+++ src/services/GoogleContactsService.js
@@ -160,3 +160,4 @@ export async function searchContacts(accessToken, query) {
 // feat: add contact import from device 7
 // feat: add contact import from device 8
 // feat: add contact import from device 9
+// feat: add favorites section to contacts 1
