e0d897e303987a3e49ee02ee3b1efb2881d67a38
Aklile612 <aklilemente72@gmail.com>
2025-10-19 02:00:00 +0300

    refactor: simplify contact storage logic

--- src/services/GoogleContactsService.js
+++ src/services/GoogleContactsService.js
@@ -238,3 +238,4 @@ export async function searchContacts(accessToken, query) {
 // feat: add emergency contact quick dial 6
 // feat: add emergency contact quick dial 7
 // refactor: simplify contact storage logic 1
+// refactor: simplify contact storage logic 2
