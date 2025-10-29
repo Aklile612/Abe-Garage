08dbf0f3cf522a4178cd2e7cb79ecb983a2e7522
Aklile612 <aklilemente72@gmail.com>
2025-10-29 16:00:00 +0300

    feat: add contact import from device

--- src/services/GoogleContactsService.js
+++ src/services/GoogleContactsService.js
@@ -156,3 +156,4 @@ export async function searchContacts(accessToken, query) {
 // feat: add contact import from device 3
 // feat: add contact import from device 4
 // feat: add contact import from device 5
+// feat: add contact import from device 6
